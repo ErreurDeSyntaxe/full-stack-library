1c265f82429be32e8c5d981e1492c49fc443d54a
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-09-27 15:37:38 +0800

    Use ES6 Classes & JS best Prac

--- script.js
+++ script.js
@@ -1,122 +1,141 @@
-const library = [];
-const bookshelf = document.querySelector('.bookshelf');
-const dialog = document.querySelector('.dialog');
-const addBook = document.querySelector('.btn-form-open');
-const confirmBook = document.querySelector('#confirm-button');
-const inTitle = document.querySelector('#title');
-const inAuthor = document.querySelector('#author');
-const inPageCount = document.querySelector('#page-count');
-const wasRead = document.querySelector('#wasRead');
-const addBookForm = document.getElementById('add-book-form');
+'use strict';
 
-addBook.addEventListener('click', () => {
-  dialog.showModal();
-});
-
-confirmBook.addEventListener('click', (event) => {
-  if (
-    inTitle.value == '' ||
-    inAuthor.value == '' ||
-    inPageCount.value == '' ||
-    inPageCount.value > 10000 ||
-    inPageCount.value < 1
-  )
-    return;
-  else submitBook();
-});
-
-function submitBook() {
-  let newBook = new Book(
-    inTitle.value,
-    inAuthor.value,
-    inPageCount.value,
-    wasRead.checked
-  );
-  library.push(newBook);
-  displayBook(newBook);
-
-  dialog.close();
-  addBookForm.reset();
-}
-
-// BOOK CONSTRUCTOR
-function Book(title, author, pageCount, read) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.read = read;
-}
-
-function displayBook(newBook) {
-  // CREATE THE DOM ELEMENTS NEEDED
-  let aBook = document.createElement('div');
-  let title = document.createElement('p');
-  let author = document.createElement('p');
-  let pageCount = document.createElement('p');
-  let readButton = document.createElement('button');
-  let lineBreak = document.createElement('br');
-  let rmvButton = document.createElement('button');
-
-  title.textContent = newBook.title;
-  title.style.fontStyle = 'italic';
-  author.textContent = newBook.author;
-  pageCount.textContent = newBook.pageCount + ' pages';
-  if (newBook.read) {
-    readButton.textContent = 'Read';
-    readButton.classList.toggle('read-button');
-  } else {
-    readButton.textContent = 'Not Read';
-    readButton.classList.toggle('notread-button');
+class Book {
+  constructor(title, author, pageCount, readStatus) {
+    this.id = Math.random().toString().slice(-8);
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+    console.log('new Book created');
+    console.log(this);
   }
-  rmvButton.textContent = 'Remove';
-
-  // add classes so the CSS takes effect
-  aBook.classList.toggle('book-card');
-  title.classList.toggle('centered');
-  author.classList.toggle('centered');
-  pageCount.classList.toggle('centered');
-  rmvButton.classList.toggle('rmv-button');
-
-  // activate the buttons on each book-card
-  readButton.addEventListener('click', () => {
-    readButton.classList.toggle('read-button');
-    readButton.classList.toggle('notread-button');
-    if (readButton.textContent == 'Read') {
-      readButton.textContent = 'Not Read';
-    } else {
-      readButton.textContent = 'Read';
-    }
-    updateRead(newBook.title);
-  });
-  rmvButton.addEventListener('click', () => {
-    aBook.remove(); //remove from DOM
-    removeBook(title.textContent); //remove from array
-  });
-
-  aBook.appendChild(title);
-  aBook.appendChild(author);
-  aBook.appendChild(pageCount);
-  aBook.appendChild(readButton);
-  aBook.appendChild(lineBreak);
-  aBook.appendChild(rmvButton);
-
-  bookshelf.appendChild(aBook);
-}
-
-function updateRead(string) {
-  for (let i = 0; i < library.length; i++) {
-    if (library[i].title === string) {
-      library[i].read = !library[i].read;
-      return;
-    }
+  updateReadStatus() {
+    this.readStatus = !this.readStatus;
   }
 }
 
-function removeBook(string) {
-  for (let i = 0; i < library.length; i++) {
-    if (library[i].title === string) {
-      library.splice(i, 1);
-      return;
+class Library {
+  #bookList = []; // where the Book objects are stored
+  constructor() {
+    this.bookshelf = document.querySelector('.bookshelf'); // where book cards are displayed
+    this.dialog = document.querySelector('.dialog'); // modal window containing the form
+    this.btnFormOpen = document.querySelector('.btn-form-open'); // btn that opens the modal
+    this.form = document.getElementById('form'); // the form
+    this.inputTitle = document.getElementById('title'); // form input
+    this.inputAuthor = document.getElementById('author'); // form input
+    this.inputPageCount = document.getElementById('page-count'); // form input
+    this.inputReadStatus = document.getElementById('read-status'); // form input
+    this.btnConfirm = document.getElementById('btn-confirm'); // btn to submit the form
+
+    // event listeners
+    this.bookshelf.addEventListener('click', (e) => this.handleClick(e));
+    this.btnFormOpen.addEventListener('click', () => this.dialog.showModal());
+    this.btnConfirm.addEventListener('click', (e) => {
+      e.preventDefault();
+      // sending an object instead of four arguments
+      const userInput = {
+        title: this.inputTitle.value,
+        author: this.inputAuthor.value,
+        pageCount: this.inputPageCount.value,
+        readStatus: this.inputReadStatus.checked,
+      };
+
+      // sending an object instead of four arguments
+      if (this.validateInput(userInput)) {
+        this.createBook(userInput);
+        this.clearModal();
+      }
+    });
+  }
+  findBookCardAndIndex(e) {
+    const target = e.target.closest('.book-card');
+    const index = this.#bookList.findIndex(
+      (book) => book.id === target.dataset.id
+    );
+    return { target, index };
+  }
+  // event delegation for changing Read/Not Read status and Removing a Book
+  handleClick(e) {
+    // Read Status
+    if (e.target?.classList.contains('read-status')) {
+      const { target, index } = this.findBookCardAndIndex(e);
+      const status = target.querySelector('.read-status');
+
+      // Reflect the changes on the DOM
+      this.#bookList[index].updateReadStatus();
+      status.classList.toggle('notread-button');
+      status.classList.toggle('read-button');
+      status.textContent =
+        status.textContent === 'Not Read' ? 'Read' : 'Not Read';
+    }
+
+    // Remove the Book from DOM and Library
+    if (e.target?.classList.contains('rmv-button')) {
+      const { target, index } = this.findBookCardAndIndex(e);
+      // Reflect the changes on the DOM
+      this.#bookList.splice(index, 1);
+      target.remove();
     }
   }
+  clearModal() {
+    this.form.reset();
+    this.dialog.close();
+  }
+  validateInput(userInput) {
+    const { title, author, pageCount } = userInput;
+    return title !== '' && author !== '' && pageCount !== '' && pageCount > 0;
+  }
+  createBook(userInput) {
+    const { title, author, pageCount, readStatus } = userInput;
+    const book = new Book(title, author, pageCount, readStatus);
+    this.displayBook(book);
+    this.#bookList.push(book);
+    console.log(this.#bookList);
+  }
+  displayBook(bookObject) {
+    const { id, title, author, pageCount, readStatus } = bookObject;
+    const bookHtml = `
+      <div class="book-card" data-id="${id}">
+        <p class="centered" style="font-style: italic;">${title}</p>
+        <p class="centered">${author}</p>
+        <p class="centered">${pageCount} pages</p>
+        <button class="${
+          readStatus ? 'read-button' : 'notread-button'
+        } read-status">${readStatus ? 'Read' : 'Not Read'}</button>
+        <br>
+        <button class="rmv-button">Remove</button>
+      </div>`;
+    this.bookshelf.insertAdjacentHTML('beforeend', bookHtml);
+  }
 }
+
+const myLibrary = new Library();
+const temp1 = {
+  title: 'The Hobbit',
+  author: 'Tolkien',
+  pageCount: 321,
+  readStatus: true,
+};
+const temp2 = {
+  title: 'Neverwhere',
+  author: 'Gaiman',
+  pageCount: 564,
+  readStatus: true,
+};
+const temp3 = {
+  title: 'The Secret',
+  author: 'Whocares',
+  pageCount: 222,
+  readStatus: false,
+};
+const temp4 = {
+  title: 'Dune',
+  author: 'Herbert',
+  pageCount: 586,
+  readStatus: true,
+};
+myLibrary.createBook(temp1);
+myLibrary.createBook(temp2);
+myLibrary.createBook(temp3);
+myLibrary.createBook(temp4);
